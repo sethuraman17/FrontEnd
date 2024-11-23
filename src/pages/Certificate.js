c54431442a77700b5499605e83e9d6fa87bef887
GRADIgithub <abipraveenraj2003@gmail.com>
2024-11-23 13:54:02 +0530

    first commit

---
+++ src/pages/Certificate.js
@@ -0,0 +1,15 @@
+import './Certificate.css';
+import img1 from './images/cert.png'
+const Services = () => {
+    return (
+        <div className="certificate-container">
+            <img 
+                src={img1} 
+                alt={'loading...'} 
+                className="full-screen-image" 
+            />
+        </div>
+    );
+}
+
+export default Services;
---
+++ src/pages/Infrastructure.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Services.css'; // External CSS for styling
+const Services = () => {
+  return (
+    <div className="services-container">
+   <p>404 not found</p>
+    </div>
+  );
+};
+
+export default Services;
